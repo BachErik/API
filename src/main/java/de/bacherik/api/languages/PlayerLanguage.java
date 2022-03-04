8f3cd0ac38294d5272e6dea2eef7c84088faac50
BachErik <75324423+BachErik@users.noreply.github.com>
2022-03-04 19:18:00 +0100

    add getLastEntry

--- src/main/java/de/bacherik/api/languages/PlayerLanguage.java
+++ src/main/java/de/bacherik/api/languages/PlayerLanguage.java
@@ -4,6 +4,9 @@ import de.niklas.nikapi.spigot.config.YAMLConfig;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 
 public class PlayerLanguage {
@@ -43,6 +46,35 @@ public class PlayerLanguage {
         getConfig().save();
         getConfig().addEntry(player, configDestination);
         getConfig().save();
+    }
+
+    public String getLastEntry(String configDestination){
+        String file = "test.txt";
+        BufferedReader breader = null;
+        try {
+            breader = new BufferedReader(new FileReader(file));
+        } catch (FileNotFoundException e1) {
+            e1.printStackTrace();
+        }
+
+        String line = null;
+        String merke = null;
+
+        try {
+            while ((line = breader.readLine()) != null) {
+                merke = line;
+            }
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+
+        try {
+            breader.close();
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+        String entry = merke.toString();
+        return entry;
     }
 
     public String setMessage(String message, String player) throws IOException {
